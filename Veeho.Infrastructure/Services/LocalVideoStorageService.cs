ee66ff3927506ab85858a74148edd0b814402f45
waqar.laghari <waqar.laghari@bankalfalah.com>
2026-04-14 18:35:21 +0500

    First Push

--- Veeho.Infrastructure/Services/LocalVideoStorageService.cs
+++ Veeho.Infrastructure/Services/LocalVideoStorageService.cs
@@ -4,6 +4,17 @@ namespace Veeho.Infrastructure.Services
 {
     public class LocalVideoStorageService : IVideoStorageService
     {
-        
+        public Task<string> SaveAsync(Stream videoStream, string fileName, string userId)
+        {
+
+        }
+        public Task<Stream> GetAsync(string videoId)
+        {
+
+        }
+        public Task<bool> DeleteAsync(string videoId)
+        {
+
+        }
     }
 }
